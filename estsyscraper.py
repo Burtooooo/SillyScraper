14126e4d0a925dcb520b61646020f7e342becca1
Burtooooo <80594530+Burtooooo@users.noreply.github.com>
2023-10-03 21:16:57 -0400

    ;etzg ooo

--- estsyscraper.py
+++ estsyscraper.py
@@ -5,16 +5,24 @@ import csv
 import time
 import json
 import string
+import os
+import re
 
+#Replace this url with an Etsy website you want to scrape
+#Make sure its the home page of a store, eg: https://www.etsy.com/shop/ashjairocreations
 URL = "https://www.etsy.com/shop/ashjairocreations"
 
-#image title price description
+ROOTDIR = "./EtsyData"
+try:
+    os.mkdir(ROOTDIR)
+except:
+    print("rootdir exists :DD")
 
 service = Service()
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--log-level=3")
 driver = webdriver.Chrome(service=service, options=options)
 printable = set(string.printable)
@@ -23,7 +31,6 @@ driver.get(URL)
 product_elements = driver.find_elements(By.CSS_SELECTOR, ".v2-listing-card")
 
 product_data = []
-
 product_links = []
 
 # Extract links to each product
@@ -34,6 +41,7 @@ for product_element in product_elements:
 for link in product_links:
     driver.get(link)
     time.sleep(2)
+    
     image_link = []
     title = driver.find_element(By.CSS_SELECTOR, "h1.wt-text-body-01").text
     price = driver.find_element(By.CSS_SELECTOR, ".wt-text-title-largest").text
@@ -41,7 +49,12 @@ for link in product_links:
     images = driver.find_elements(By.CSS_SELECTOR, ".listing-page-image-carousel-component .carousel-image")
     for image in images:
         image_link.append(image.get_attribute("src"))
-    print(title)
-    print(price)
-    print(desc)
-    time.sleep(4)
+    path_title = re.sub(r'[^\w_. -]', '_', title)
+    mydir = ROOTDIR + "/" + path_title
+    os.mkdir(mydir)
+    file1 = open(mydir + "/info.txt","w")
+    file1.write(title)
+    file1.write(price + "\n")
+    file1.write(desc)
+    
+    #time.sleep(4)
