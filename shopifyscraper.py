5da979886800d108130c62576b6799976a4c6323
Burtooooo <80594530+Burtooooo@users.noreply.github.com>
2023-10-05 20:32:31 -0400

    fix

--- shopifyscraper.py
+++ shopifyscraper.py
@@ -21,7 +21,7 @@ service = Service()
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--log-level=3")
 driver = webdriver.Chrome(service=service, options=options)
 printable = set(string.printable)
@@ -36,36 +36,44 @@ product_links = []
 for product_element in product_elements:
     product_link = product_element.get_attribute("href")
     product_links.append(product_link)
+print(len(product_links))
 
 for link in product_links:
     driver.get(link)
     time.sleep(2)
     
-    image_link = []
-    title = driver.find_element(By.CSS_SELECTOR, ".product-single__title").text
-    price = driver.find_element(By.CSS_SELECTOR, ".product-single__price").text
-    desc = driver.find_element(By.CSS_SELECTOR, ".rte p").text
-    images = driver.find_elements(By.CSS_SELECTOR, ".js-modal-open-product-modal")
-    for image in images:
-        image_link.append(image.get_attribute("href"))
-    path_title = re.sub(r'[^\w_. -]', '_', title)
-    mydir = ROOTDIR + "/" + path_title
-    try: 
-        os.mkdir(mydir)
-    except:
-        pass
-    file1 = open(mydir + "/info.txt","w")
-    file1.write(title + "\n")
-    file1.write(price + "\n")
-    file1.write(desc)
-    file1.close()
+    try:
+        image_link = []
+        title = driver.find_element(By.CSS_SELECTOR, ".product-single__title").text
+        price = driver.find_element(By.CSS_SELECTOR, ".product-single__price").text
+        desc = ""
+        try:
+            desc = driver.find_element(By.CSS_SELECTOR, ".rte p").text
+        except:
+            desc = ""
+        images = driver.find_elements(By.CSS_SELECTOR, ".js-modal-open-product-modal")
+        for image in images:
+            image_link.append(image.get_attribute("href"))
+        path_title = re.sub(r'[^\w_. -]', '_', title)
+        mydir = ROOTDIR + "/" + path_title
+        try: 
+            os.mkdir(mydir)
+        except:
+            pass
+        file1 = open(mydir + "/info.txt","w")
+        file1.write(title + "\n")
+        file1.write(price + "\n")
+        file1.write(desc)
+        file1.close()
 
-    for i in range(len(image_link)):
-        data = requests.get(image_link[i]).content
-        index = i-1
-        if index == -1:
-            index = ""
-        f = open(mydir + "/image" + str(index) + ".jpg", 'wb')
-        f.write(data)
-        f.close()
-    time.sleep(.5)
+        for i in range(len(image_link)):
+            data = requests.get(image_link[i]).content
+            index = i-1
+            if index == -1:
+                index = ""
+            f = open(mydir + "/image" + str(index) + ".jpg", 'wb')
+            f.write(data)
+            f.close()
+        time.sleep(.5)
+    except:
+        print("oopsie poopsie")
